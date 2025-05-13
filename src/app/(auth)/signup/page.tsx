d7cc8aa196e2982997aa72c5a2b9af90308a98cd
EliãFraga <fragadesenestu01@gmail.com>
2025-05-12 22:36:43 -0300

    chore: finalizado a validação no app

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -11,9 +11,49 @@ import { useState } from 'react';
 
 import Feather from '@expo/vector-icons/Feather';
 
+import { z } from 'zod';
+
+import { useForm, Controller } from 'react-hook-form';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const authSchema = z.object({
+    name: z
+        .string()
+        .nonempty("O campo de nome é obrigatório.")
+        .min(3, "O nome deve ter pelo menos 3 caracteres.")
+        .max(100, "O nome deve ter no máximo 100 caracteres."),
+    email: z
+        .string()
+        .nonempty("O campo de email é obrigatório.")
+        .email("O email deve ser válido."),
+    password: z
+        .string()
+        .nonempty("O campo de senha é obrigatório.")
+        .min(6, "A senha deve ter pelo menos 6 caracteres.")
+        .regex(/[A-Z]/, "A senha deve conter pelo menos uma letra maiúscula.")
+        .regex(/[!@#$%^&*(),.?":{}|<>]/, "A senha deve conter pelo menos um caractere especial.")
+        .regex(/[0-9]/, "A senha deve conter pelo menos um número."),
+})
+
+type AuthSchema = z.infer<typeof authSchema>;
+
 export default function SignUp() {
 
     const [visiblePassword, setVisiblePassword] = useState(true);
+
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<AuthSchema>({
+        resolver: zodResolver(authSchema)
+    });
+
+    function proxPagina() {
+        console.log("Cadastro realizado com sucesso!");
+    }
+
 
     return (
         <SafeAreaView style={styles.container}>
@@ -24,11 +64,24 @@ export default function SignUp() {
 
                 <Text style={styles.label}>Nome:</Text>
 
-                <TextInput
-                    style={styles.input}
-                    placeholder='Digite seu nome:'
-                    placeholderTextColor={'#D7E2EA'}
+                <Controller
+                    control={control}
+                    name="name"
+                    render={({
+                        field: { value, onChange, onBlur },
+                    }) => (
+                        <TextInput
+                            style={styles.input}
+                            placeholder='Digite seu nome:'
+                            placeholderTextColor={'#D7E2EA'}
+                            value={value}
+                            onChangeText={onChange}
+                            onBlur={onBlur}
+                        />
+                    )}
                 />
+
+                {errors.name && <Text style={{ color: 'red' }}>{errors.name.message}</Text>}
 
             </View>
 
@@ -36,11 +89,24 @@ export default function SignUp() {
 
                 <Text style={styles.label}>Email:</Text>
 
-                <TextInput
-                    style={styles.input}
-                    placeholder='Digite seu email:'
-                    placeholderTextColor={'#D7E2EA'}
+                <Controller
+                    control={control}
+                    name="email"
+                    render={({
+                        field: { value, onChange, onBlur },
+                    }) => (
+                        <TextInput
+                            style={styles.input}
+                            placeholder='Digite seu email:'
+                            placeholderTextColor={'#D7E2EA'}
+                            value={value}
+                            onChangeText={onChange}
+                            onBlur={onBlur}
+                        />
+                    )}
                 />
+
+                {errors.email && <Text style={{ color: 'red' }}>{errors.email.message}</Text>}
 
             </View>
 
@@ -50,11 +116,20 @@ export default function SignUp() {
 
                 <View style={styles.areaInputIcon}>
 
-                    <TextInput
-                        style={styles.inputSenha}
-                        placeholder='Digite sua senha:'
-                        placeholderTextColor={'#D7E2EA'}
-                        secureTextEntry={visiblePassword}
+                    <Controller
+                        control={control}
+                        name="password"
+                        render={({ field: { value, onChange, onBlur } }) => (
+                            <TextInput
+                                style={styles.inputSenha}
+                                placeholder='Digite sua senha:'
+                                placeholderTextColor={'#D7E2EA'}
+                                secureTextEntry={visiblePassword}
+                                value={value}
+                                onChangeText={onChange}
+                                onBlur={onBlur}
+                            />
+                        )}
                     />
 
                     <TouchableOpacity style={styles.buttonEye} activeOpacity={1.0} onPress={() => { setVisiblePassword(!visiblePassword) }}>
@@ -69,9 +144,11 @@ export default function SignUp() {
 
                 </View>
 
+                {errors.password && <Text style={{ color: 'red' }}>{errors.password.message}</Text>}
+
             </View>
 
-            <TouchableOpacity style={styles.button} activeOpacity={0.5}>
+            <TouchableOpacity style={styles.button} activeOpacity={0.5} onPress={handleSubmit(proxPagina)}>
 
                 <Text style={styles.textButton}>Cadastrar</Text>
 
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,6 +3,7 @@ import {
     Text,
     StyleSheet,
     SafeAreaView,
+    StatusBar,
     Image,
     TextInput,
     ScrollView,
@@ -15,13 +16,54 @@ import Feather from '@expo/vector-icons/Feather';
 
 import { Link } from 'expo-router';
 
+import { z } from 'zod';
+
+import { useForm, Controller } from 'react-hook-form';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { useRouter } from 'expo-router';
+
+const authSchema = z.object({
+    email: z
+        .string()
+        .nonempty("O campo de email é obrigatório.")
+        .email("O email deve ser válido."),
+    password: z
+        .string()
+        .nonempty("O campo de senha é obrigatório.")
+        .min(6, "A senha deve ter pelo menos 6 caracteres.")
+        .regex(/[A-Z]/, "A senha deve conter pelo menos uma letra maiúscula.")
+        .regex(/[!@#$%^&*(),.?":{}|<>]/, "A senha deve conter pelo menos um caractere especial.")
+        .regex(/[0-9]/, "A senha deve conter pelo menos um número."),
+})
+
+type AuthSchema = z.infer<typeof authSchema>;
+
+
 export default function Login() {
 
     const [visiblePassword, setVisiblePassword] = useState(true);
 
+    const router = useRouter();
+
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<AuthSchema>({
+        resolver: zodResolver(authSchema)
+    });
+
+    function proxPagina() {
+        router.push('/(loggedin)/user/page');
+    }
+
     return (
 
         <SafeAreaView style={styles.container}>
+
+            <StatusBar barStyle='dark-content' backgroundColor='#D7E2EA' />
 
             <Image source={require('../assets/image/logo.png')} style={{ height: 250, width: 350 }} />
 
@@ -31,11 +73,25 @@ export default function Login() {
 
                     <Text style={styles.label}>Email:</Text>
 
-                    <TextInput
-                        style={styles.input}
-                        placeholder='Digite seu email:'
-                        placeholderTextColor={'#D7E2EA'}
+                    <Controller
+                        control={control}
+                        name="email"
+                        render={({
+                            field: { value, onChange, onBlur },
+                        }) => (
+                            <TextInput
+                                style={styles.input}
+                                placeholder='Digite seu email:'
+                                placeholderTextColor={'#D7E2EA'}
+                                value={value}
+                                onChangeText={onChange}
+                                onBlur={onBlur}
+                            />
+                        )}
                     />
+
+                    {errors.email && <Text style={{ color: "red", marginTop: 5 }}>{errors.email.message}</Text>}
+
                 </View>
 
                 <View style={styles.areaLabelInput}>
@@ -44,11 +100,20 @@ export default function Login() {
 
                     <View style={styles.areaInputIcon}>
 
-                        <TextInput
-                            style={styles.inputSenha}
-                            placeholder='Digite sua senha:'
-                            placeholderTextColor={'#D7E2EA'}
-                            secureTextEntry={visiblePassword}
+                        <Controller
+                            control={control}
+                            name="password"
+                            render={({ field: { value, onChange, onBlur } }) => (
+                                <TextInput
+                                    style={styles.inputSenha}
+                                    placeholder='Digite sua senha:'
+                                    placeholderTextColor={'#D7E2EA'}
+                                    secureTextEntry={visiblePassword}
+                                    value={value}
+                                    onChangeText={onChange}
+                                    onBlur={onBlur}
+                                />
+                            )}
                         />
 
                         <TouchableOpacity style={styles.buttonEye} activeOpacity={1.0} onPress={() => { setVisiblePassword(!visiblePassword) }}>
@@ -62,9 +127,12 @@ export default function Login() {
                         </TouchableOpacity>
 
                     </View>
+
+                    {errors.password && <Text style={{ color: "red", marginTop: 5 }}>{errors.password.message}</Text>}
+
                 </View>
 
-                <TouchableOpacity style={styles.button} activeOpacity={0.5}>
+                <TouchableOpacity style={styles.button} activeOpacity={0.5} onPress={handleSubmit(proxPagina)}>
 
                     <Text style={styles.textButton}>Acessar</Text>
 
@@ -73,6 +141,19 @@ export default function Login() {
                 <Link href={'(auth)/signup/page'}>
                     <Text style={styles.link}>Ainda não possui uma conta? Cadastra - se</Text>
                 </Link>
+
+                <Text style={{ marginVertical: 15, fontSize: 18, color: "#5D656C" }}>or</Text>
+
+                <TouchableOpacity style={styles.buttonAuth} activeOpacity={0.5}>
+
+                    <Image
+                        source={require('../assets/image/logogoogle.png')}
+                        style={{ height: 45, width: 35 }}
+                    />
+
+                    <Text style={styles.textButton}>Entrar com Google</Text>
+
+                </TouchableOpacity>
 
             </ScrollView>
 
@@ -108,7 +189,7 @@ const styles = StyleSheet.create({
         padding: 10,
         color: "#D7E2EA",
     },
-    inputSenha:{
+    inputSenha: {
         width: "85%",
         height: 43,
         backgroundColor: "#5D656C",
@@ -140,5 +221,15 @@ const styles = StyleSheet.create({
         fontSize: 17,
         color: "#5D656C",
         fontWeight: "bold",
+    },
+    buttonAuth: {
+        width: "60%",
+        height: 43,
+        backgroundColor: "#5D656C",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "row",
+        gap: 10,
+        borderRadius: 8,
     }
 });
